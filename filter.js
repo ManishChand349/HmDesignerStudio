b19305fad87c40dc9751424ce2b5e8cb53686ad2
Manish Chand <manishchandr349@gmail.com>
2023-09-26 23:10:45 +0530

    adding file

---
+++ filter.js
@@ -0,0 +1,172 @@
+const data = [
+     {
+       id: 1,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-1.fna.fbcdn.net/v/t39.30808-6/378583488_765761392232640_9050060757948677751_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=KzWZOGrmMn8AX-y22hu&edm=ACWDqb8AAAAA&ccb=7-5&ig_cache_key=MzE5NDQ3NDI1MjUyMjA5MTc1NA%3D%3D.2-ccb7-5&oh=00_AfA4xKwq51TwxhON5O1mGIufWIrT9WRYfCQiq9Xn87Iclw&oe=65185334&_nc_sid=ee9879",
+       price: 14990,
+       cat: "Lehengas",
+     },
+     {
+       id: 2,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-2.fna.fbcdn.net/v/t39.30808-6/378483587_764659092342870_6952969521371826337_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=gT_suQ6qAJcAX93vPs4&edm=ACWDqb8AAAAA&ccb=7-5&ig_cache_key=MzE5MzExMzU4NzMxNzI0MDg5MQ%3D%3D.2-ccb7-5&oh=00_AfC3hWNsUf_e8Qep3U5wwVI0pbeCcna2VW7kdGn16ouhjw&oe=651696AD&_nc_sid=ee9879",
+       price: 17990,
+       cat: "Lehengas",
+     },
+     {
+       id: 3,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-1.fna.fbcdn.net/v/t39.30808-6/379731414_767056698769776_1191543913300200424_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=cbW67wW6H9wAX_2wVwG&edm=ACWDqb8AAAAA&ccb=7-5&ig_cache_key=MzE5NTk4NTcwNzQyMzY0Njg3Mg%3D%3D.2-ccb7-5&oh=00_AfBQ9msGvFyTxBLH7dvmDqNDP9RoYVd5zHhD4AITkWalnQ&oe=651789AF&_nc_sid=ee9879",
+       price: 15990,
+       cat: "Lehengas",
+     },
+     {
+       id: 4,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-4.fna.fbcdn.net/v/t39.30808-6/376748497_760470689428377_3222780761610667648_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-4.fna.fbcdn.net&_nc_cat=109&_nc_ohc=IbbntNi-fVkAX-OI_7r&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE4ODQ4MTM3NjU2NzI2NjcwMw%3D%3D.2-ccb7-5&oh=00_AfDMm17qKaF7j4wYwRqk09oIhrhWLnrZ4MpQeWx3w-b_6g&oe=651618EA&_nc_sid=b41fef",
+       price: 18990,
+       cat: "Lehengas",
+     },
+     {
+       id: 3,
+       name: "Dresses",
+       img: "https://instagram.fdel11-2.fna.fbcdn.net/v/t39.30808-6/371895095_756046213204158_1758754581947459483_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=l-KxvWozse0AX8j-C7F&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE4MzM5Mjc0NTM4NTc2OTQ5MA%3D%3D.2-ccb7-5&oh=00_AfAKbuRWcLz2wjrIRVZeCbAG6FAn2naipEETzgrnm3jugw&oe=6517F22C&_nc_sid=b41fef",
+       price: 17990,
+       cat: "Dresses",
+     },
+     {
+       id: 3,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-4.fna.fbcdn.net/v/t39.30808-6/366734892_728258492649597_9119725352913693444_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-4.fna.fbcdn.net&_nc_cat=109&_nc_ohc=bLBfTsUqDmAAX-x7WAL&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE3MDY0OTEyMjgxMzkyOTc2MA%3D%3D.2-ccb7-5&oh=00_AfDX4fjDtWE2GPBcOeEWQERidp5jIwRxT_btV2VFByLuFg&oe=65187C46&_nc_sid=b41fef",
+       price: 15990,
+       cat: "Lehengas",
+     },
+     {
+       id: 3,
+       name: "Dresses",
+       img: "https://instagram.fdel11-4.fna.fbcdn.net/v/t39.30808-6/364105201_693690136106433_7288847794313738205_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-4.fna.fbcdn.net&_nc_cat=109&_nc_ohc=KIfvDbu501gAX8mwXYA&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE2MDYyMjcwNzE5NzEwMjYxOA%3D%3D.2-ccb7-5&oh=00_AfDCfQjbURBw9ylxP7tKwfCeU1oXyh_3JUZvM9O30diMBg&oe=65180689&_nc_sid=b41fef",
+       price: 19990,
+       cat: "Dresses",
+     },
+     {
+          id: 3,
+          name: "Dresses",
+          img: "https://instagram.fdel11-2.fna.fbcdn.net/v/t39.30808-6/359368902_3136853613287455_4595320038610438502_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Z8z2kiNC0nYAX9Y1542&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE0NzE2OTAxODkzNTEwMDI3Nw%3D%3D.2-ccb7-5&oh=00_AfDHcBLhru1AvH_zwuklZDaKdObc933QD2EmaS_RM1NXdQ&oe=6517A3DE&_nc_sid=b41fef",
+          price: 19990,
+          cat: "Dresses",
+        },
+     {
+       id: 3,
+       name: "Lehengas",
+       img: "https://instagram.fdel11-2.fna.fbcdn.net/v/t39.30808-6/364797886_694666369342143_8621013116289478883_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=0p3tHVFwE98AX-BIlCi&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE2MTEyNzczMTEzMTE5MTM0OQ%3D%3D.2-ccb7-5&oh=00_AfDtTXQDpgcvGO9LFLDEgJe9yTDHSgRgIP384prUNjE5hg&oe=6515C09D&_nc_sid=b41fef",
+       price: 19990,
+       cat: "Lehengas",
+     },
+     {
+       id: 3,
+       name: "Gowns",
+       img: "https://instagram.fdel11-2.fna.fbcdn.net/v/t39.30808-6/364738227_693682929440487_5488469588227291328_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=5SnnChCAD0IAX-uyQao&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE2MDM5MjY1NTIzNDg1MTYzMw%3D%3D.2-ccb7-5&oh=00_AfA1jaAr5JPnO7985KCNSijv9TB1kNp320kqc7Zp6uzZ5g&oe=6515885B&_nc_sid=b41fef",
+       price: 19990,
+       cat: "Gowns",
+     },
+     {
+       id: 3,
+       name: "Gowns",
+       img: "https://instagram.fdel11-4.fna.fbcdn.net/v/t39.30808-6/362208221_685316736943773_1854973410411330500_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-4.fna.fbcdn.net&_nc_cat=109&_nc_ohc=FkEOkH1_HVkAX8hdfmz&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE1MTUxNzczMTQ1NTIxNjEwMw%3D%3D.2-ccb7-5&oh=00_AfCVWM3_FovZyPVUjQrbI-CKiRZdkQA20OsbWh5O3tK-Rg&oe=65176B44&_nc_sid=b41fef",
+       price: 19990,
+       cat: "Gowns",
+     },
+     {
+       id: 3,
+       name: "Gowns",
+       img: "https://instagram.fdel11-3.fna.fbcdn.net/v/t39.30808-6/358379200_3136854393287377_8787915452789333356_n.jpg?stp=dst-jpg_e15&_nc_ht=instagram.fdel11-3.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qy4SqMy9c6oAX_-kiSH&edm=ABmJApAAAAAA&ccb=7-5&ig_cache_key=MzE0NzU0NjUwMzk4ODMwMzg3NA%3D%3D.2-ccb7-5&oh=00_AfAsfVSkbbDqAoyEB0q3YdQ7U772jY5eCgoXH9glqb6Qjw&oe=65185983&_nc_sid=b41feff",
+       price: 19990,
+       cat: "Gowns",
+     },
+     
+     
+     
+     
+      
+     
+     
+     
+   ];
+   
+   const productsContainer = document.querySelector(".products");
+   const searchInput = document.querySelector(".searchInput");
+   const categoriesContainer = document.querySelector(".cats");
+   const priceRange = document.querySelector(".priceRange");
+   const priceValue = document.querySelector(".priceValue");
+   
+   const displayProducts = (filteredProducts) => {
+     productsContainer.innerHTML = filteredProducts
+       .map(
+         (product) =>
+           `
+             <div class="product">
+                            <img class="img1" src=${product.img} alt="">
+                            <span class="name">${product.name}</span>
+                            <span class="priceText">₹ ${product.price}</span>
+                       </div>
+   
+             `
+       )
+       .join("");
+   };
+   displayProducts(data);
+   
+   searchInput.addEventListener("keyup", (e) => {
+     const value = e.target.value.toLowerCase();
+   
+     if (value) {
+       displayProducts(
+         data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+       );
+     } else {
+       displayProducts(data);
+     }
+   });
+   
+   const setCategories = () => {
+     const allCats = data.map((item) => item.cat);
+     const categories = [
+       "All",
+       ...allCats.filter((item, i) => {
+         return allCats.indexOf(item) === i;
+       }),
+     ];
+   
+     categoriesContainer.innerHTML = categories
+       .map(
+         (cat) => `
+        <span class="cat">${cat}</span>
+     `
+       )
+       .join("");
+   
+     categoriesContainer.addEventListener("click", (e) => {
+       const selectedCat = e.target.textContent;
+   
+       selectedCat === "All"
+         ? displayProducts(data)
+         : displayProducts(data.filter((item) => item.cat === selectedCat));
+     });
+   };
+   const setPrice = () => {
+     const priceList = data.map((item) => item.price);
+     const minPrice = Math.min(...priceList);
+     const maxPrice = Math.max(...priceList);
+   
+     priceRange.min = minPrice;
+     priceRange.max = maxPrice;
+     priceRange.value = maxPrice;
+     priceValue.textContent = "₹" + maxPrice;
+   
+     priceRange.addEventListener("input", (e) => {
+       priceValue.textContent = "₹" + e.target.value;
+       displayProducts(data.filter((item) => item.price <= e.target.value));
+     });
+   };
+   setCategories();
+   setPrice();
